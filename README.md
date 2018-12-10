6127e99ced1e6babda76df5a9b153006c874fc88
John Thompson <john@springframework.guru>
2018-12-09 19:26:54 -0500

    initial project add

---
+++ README.md
@@ -0,0 +1,1 @@
+# Testing Java with Maven Examples
---
+++ src/main/java/guru/springframework/JavaHelloWorld.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+/**
+ * Created by jt on 2018-12-09.
+ */
+public class JavaHelloWorld {
+
+    public String getHello(){
+        return "Hello World";
+    }
+
+}
