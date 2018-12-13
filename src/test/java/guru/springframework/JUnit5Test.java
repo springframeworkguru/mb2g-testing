257e3f9097bc12be2aa838aac4269e8a7bc37cc4
John Thompson <john@springframework.guru>
2018-12-12 20:25:02 -0500

    adding jUnit 5 - *** NOT WORKING *** waiting for Java 11 support in Groovy Eclipse compiler

---
+++ src/test/java/guru/springframework/JUnit5Test.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by jt on 2018-12-12.
+ */
+public class JUnit5Test {
+
+    @Test
+    void exampleTest() {
+    }
+}
