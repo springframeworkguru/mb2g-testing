7cd0e65bc065d9db4343771abeaca41a2c5d9703
John Thompson <john@springframework.guru>
2018-12-14 14:54:58 -0500

    adding failsafe

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by jt on 2018-12-14.
+ */
+public class JavaHelloWorldIT {
+
+    @Test
+    void myFauxIntegrationTest() {
+
+        System.out.println("My IT Ran");
+    }
+}
