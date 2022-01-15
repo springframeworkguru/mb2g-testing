c48226862956f4808f2edbaca428891d4504ae9a
Oleksii Riabov <ryaboff99@gmail.com>
2022-01-15 23:05:05 +0200

    Add JavaHelloWorldTest.java - a POJO test for JavaHelloWorldTest.java

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+
+public class JavaHelloWorldTest {
+
+    public void testGetHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assert ("Hello World".equals(javaHelloWorld.getHello()));
+    }
+}
