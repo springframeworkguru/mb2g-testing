04dd12ec67e531dcc7501894b4bca3f233fc4207
Oleksii Riabov <ryaboff99@gmail.com>
2022-01-18 20:23:21 +0200

    Add JUnit5FauxTest.java.java to run junit 5 test

---
+++ src/test/java/guru/springframework/JUnit5FauxTest.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5FauxTest {
+    @Test
+    void someTestForJUnit5() {
+        System.out.println("I Ran...");
+    }
+}
