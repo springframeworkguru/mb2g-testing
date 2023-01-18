65706dcab973193a35f79b8d6d263f1ea4108f41
Jörg-Christian Müller <jcm@shv-is.eu>
2023-01-18 12:16:48 +0100

    End Lesson 72: Surefire => Junit 4 and 5

--- src/test/java/guru/springframework/JUnit4HelloWorldTest.java
+++ src/test/java/guru/springframework/JUnit4HelloWorldTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class JavaHelloWorldTest {
+public class JUnit4HelloWorldTest {
 
     @Test
     public void getHello() {
---
+++ src/test/java/guru/springframework/JUnit5FauxTestTest.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JUnit5FauxTestTest {
+    @Test
+    public void someTestforJUnit5() {
+        System.out.println("I ran ....");
+    }
+}
